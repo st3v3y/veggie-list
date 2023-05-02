17ee1bd9287cd7d7dd5afcdda1da0897602c5164
Stefan Lessle <stefan.lessle@mediakular.com>
2023-05-02 15:11:17 -0500

    Minor mobile improvements

--- apps/veggie-list/src/lib/components/veggies/veggie-order.svelte
+++ apps/veggie-list/src/lib/components/veggies/veggie-order.svelte
@@ -22,7 +22,7 @@
         duration: 1000,
         easing: quartInOut,
     });
-    const initImgSize =  veggie.is_favorite ? 80 : 60;
+    const initImgSize =  veggie.is_favorite ? 60 : 40;
     const imgSize = tweened(initImgSize, {
         duration: 1000,
         easing: quartInOut,
@@ -33,18 +33,18 @@
             checkColor.set([73, 222, 128]);
             checkSize.set(35);
             boxPadding.set(30);
-            imgSize.set(80);
+            imgSize.set(60);
         } else {
             checkColor.set([148, 163, 184]);
             checkSize.set(25);
             boxPadding.set(10);
-            imgSize.set(60);
+            imgSize.set(40);
         }
     }
 </script>
 
 <div class="shadow-veggie flex flex-row items-center justify-between max-w-[600px] w-full mb-6 px-5 bg-[#F9FAFC] gap-5 rounded-xl" class:opacity-40={!veggie.is_favorite} style:padding-top={`${$boxPadding}px`} style:padding-bottom={`${$boxPadding}px`}>
-    <div class="w-[80px] flex justify-center flex-start">
+    <div class="w-[60px] flex justify-center flex-start">
         <div class="rounded-full overflow-hidden" style:width={`${$imgSize}px`} style:height={`${$imgSize}px`}>
             <input class="object-cover h-full w-full cursor-default" type="image" src={veggie.picture_url} alt={veggie.name}/>
         </div>
@@ -61,21 +61,23 @@
             <span class="text-[#AAA]">${veggie.price} / {$_(`constants.units.singular.${veggie.unit}`)}</span>
         </div>
         {#if veggie.is_favorite}
-            <div transition:fade class="flex justify-start w-full gap-4">
+            <div transition:fade class="flex justify-start flex-wrap md:flex-nowrap w-full gap-4">
                 <input name="amount" bind:value={veggie.amount} class="form-input w-full px-4 py-3 rounded-lg border border-slate-400" aria-label="Amount" placeholder={$_("page.order.form.amount.placeholder")}  />
-                <div class="min-w-[120px]">
-                    <select name="order_unit" bind:value={veggie.order_unit} class="form-input w-full px-3 py-3 rounded-lg border border-slate-400">
-                        <option value="unit">{$_('constants.units.singular.unit')}</option>
-                        <option value="dollar">{$_('constants.units.singular.dollar')}</option>
-                        <option value="libra">{$_('constants.units.singular.libra')}</option>
-                        <option value="kilogram">{$_('constants.units.singular.kilogram')}</option>
-                        <option value="gram">{$_('constants.units.singular.gram')}</option>
-                    </select>
-                </div>
-                <div class="flex items-center">
-                    <button on:click={() => veggie.hasComment = true} disabled={veggie.hasComment}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill={veggie.hasComment ? "#dbdbdb" : "#aaa"} stroke="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-square"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>
-                    </button>
+                <div class="flex w-full gap-4">
+                    <div class="grow min-w-[120px]">
+                        <select name="order_unit" bind:value={veggie.order_unit} class="form-input w-full px-3 py-3 rounded-lg border border-slate-400">
+                            <option value="unit">{$_('constants.units.singular.unit')}</option>
+                            <option value="dollar">{$_('constants.units.singular.dollar')}</option>
+                            <option value="libra">{$_('constants.units.singular.libra')}</option>
+                            <option value="kilogram">{$_('constants.units.singular.kilogram')}</option>
+                            <option value="gram">{$_('constants.units.singular.gram')}</option>
+                        </select>
+                    </div>
+                    <div class="flex items-center">
+                        <button on:click={() => veggie.hasComment = true} disabled={veggie.hasComment}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill={veggie.hasComment ? "#dbdbdb" : "#aaa"} stroke="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-message-square"><path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path></svg>
+                        </button>
+                    </div>
                 </div>
             </div>
             {#if veggie.hasComment}
